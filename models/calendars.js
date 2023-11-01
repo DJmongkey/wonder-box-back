165e34e4157c56b5bb02f838bf66d09339448507
selene-cho <selene.cho29@gmail.com>
2023-11-01 23:35:16 +0900

    feat: daily-boxes post 추가, refactor: verifyToken 미들웨어 이용 리팩토링

--- models/calendars.js
+++ models/calendars.js
@@ -1,5 +1,40 @@
 const mongoose = require('mongoose');
 
+const boxSchema = new mongoose.Schema({
+  font: {
+    type: String,
+    required: [true, '날짜 박스 글씨체를 지정해주세요.'],
+  },
+  color: {
+    type: String,
+    required: [true, '날짜 박스 색생을 지정해주세요.'],
+  },
+  bgColor: {
+    type: String,
+    required: [true, '날짜 박스 배경 색상을 지정해주세요.'],
+  },
+});
+
+const styleSchema = new mongoose.Schema({
+  titleFont: {
+    type: String,
+    required: [true, '제목 글씨체를 지정해주세요.'],
+  },
+  titleColor: {
+    type: String,
+    required: [true, '제목 색상을 지정해주세요.'],
+  },
+  borderColor: {
+    type: String,
+    required: [true, '제목의 테두리 색상을 지정해주세요.'],
+  },
+  bgImage: {
+    type: String,
+    required: [true, '배경 이미지를 지정해주세요.'],
+  },
+  box: boxSchema,
+});
+
 const calendarSchema = new mongoose.Schema({
   userId: {
     type: mongoose.Schema.Types.ObjectId,
@@ -9,6 +44,10 @@ const calendarSchema = new mongoose.Schema({
     type: String,
     required: [true, 'WonderBox 이름을 입력해주세요.'],
     minlength: 2,
+    validate: {
+      validator: (v) => v.length >= 2,
+      message: 'WonderBox 이름은 최소 2자 이상이어야 합니다.',
+    },
   },
   creator: {
     type: String,
@@ -28,7 +67,7 @@ const calendarSchema = new mongoose.Schema({
       message: '시작일은 종료일보다 과거여야 합니다.',
     },
   },
-  style: { type: mongoose.Schema.Types.ObjectId, ref: 'Style' },
+  style: styleSchema,
   options: [
     {
       type: String,
@@ -36,7 +75,7 @@ const calendarSchema = new mongoose.Schema({
       required: true,
     },
   ],
-  contents: [{ type: mongoose.Schema.Types.ObjectId, ref: 'DailyBox' }],
+  dailyBoxes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'DailyBox' }],
   shareUrl: {
     type: String,
   },
