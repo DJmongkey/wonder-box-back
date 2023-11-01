f36e3e5684055b088e143e228f4d2ef760a88a62
selene-cho <selene.cho29@gmail.com>
2023-11-01 23:32:05 +0900

    fix: 로그인/회원가입 에러 수정

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,17 +4,19 @@ const HttpError = require('../controllers/httpError');
 const ERRORS = require('../errorMessages');
 
 exports.verifyToken = (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  const token = authHeader && authHeader.split(' ')[1];
+  const token = req.headers.authorization;
 
   if (!token) {
     return next(new HttpError(401, ERRORS.AUTH.NOT_FOUND_TOKEN));
   }
 
   return jwt.verify(token, process.env.JWT_ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) return next(new HttpError(401, ERRORS.AUTH.INVALID_ACCESS_TOKEN));
+    if (err) {
+      return next(new HttpError(401, ERRORS.AUTH.INVALID_ACCESS_TOKEN));
+    }
 
     req.user = user;
+
     return next();
   });
 };
--- models/users.js
+++ models/users.js
@@ -29,7 +29,7 @@ const userSchema = new mongoose.Schema({
   calendars: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Calendar' }],
 });
 
-userSchema.pre('save', async (next) => {
+userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
 
   const salt = await bcrypt.genSalt(11);
